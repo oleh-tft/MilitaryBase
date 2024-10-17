8dcf65f6c4f32a81dc26e1b984b7002ea1053243
oleh-tft <tiftelalbion@gmail.com>
2024-10-17 11:37:15 +0300

    v0.0

---
+++ MilitaryBase/Base.h
@@ -0,0 +1,14 @@
+#pragma once
+class Base
+{
+	static int people_on_base;
+	static int vehicles_on_base;
+	static double petrol_on_base;
+	static double goods_on_base;
+};
+
+int Base::people_on_base = 108;
+int Base::vehicles_on_base = 12;
+double Base::petrol_on_base = 16600.0;
+double Base::goods_on_base = 8120.0;
+
---
+++ MilitaryBase/Main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"Base.h"
+
+using namespace std;
+
+int main()
+{
+	Base base;
+	
+}
